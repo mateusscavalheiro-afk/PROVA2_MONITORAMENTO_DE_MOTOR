64997f178573b10ef1793df04d2f42aae007fa64
mateusscavalheiro-afk <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2026-02-24 08:40:52 -0300

    Update main.cpp

--- PlatformIO/Projects/PROVA2_MONITORAMENTO_DE_MOTOR/src/main.cpp
+++ PlatformIO/Projects/PROVA2_MONITORAMENTO_DE_MOTOR/src/main.cpp
@@ -3,25 +3,24 @@
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 
-// --- 1. CONFIGURAÇÕES (WIFI e MQTT) ---
+//1. CONFIGURAÇÕES (WIFI e MQTT)
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 const char* mqtt_server = "broker.hivemq.com";
 
-// TÓPICO OBRIGATÓRIO (Mude SEU_NOME)
 const char* topic_publish = "senai/MATEUS/motor/dados"; 
 
-// --- 2. HARDWARE (PINAGEM DO ANEXO 1) ---
+//2. HARDWARE
 #define PIN_DHT 15       // Temperatura
 #define PIN_VIB 34       // Vibração (Potenciômetro 1)
 #define PIN_COR 35       // Corrente (Potenciômetro 2)
 
-// LEDs (Sua lógica de semáforo para vibração)
+// LEDs
 #define LED_VERDE 12     // GPIO 12
-#define LED_AMARELO 14   // GPIO 14 (Adicional para alerta)
-#define LED_VERMELHO 27  // GPIO 27 (Adicional para perigo)
+#define LED_AMARELO 14   // GPIO 14 
+#define LED_VERMELHO 27  // GPIO 27 
 
-// --- 3. OBJETOS ---
+//3. ATIVAÇÃO DE OBJETOS
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(PIN_DHT, DHT22);
@@ -72,21 +71,21 @@ void loop() {
   }
   client.loop();
 
-  // --- 4. LEITURA E MAPEAMENTO ---
+  //4. LEITURA E MAPEAMENTO
   
-  // A) Temperatura
+  //Temperatura
   float temp = dht.readTemperature();
   if (isnan(temp)) temp = 0.0;
 
-  // B) Vibração (0-4095 -> 0-100%)
+  //Vibração (0-4095 -> 0-100%)
   int leituraVib = analogRead(PIN_VIB);
   int vibracao = map(leituraVib, 0, 4095, 0, 100);
 
-  // C) Corrente (0-4095 -> 0-100 A)
+  //Corrente (0-4095 -> 0-100 A)
   int leituraCor = analogRead(PIN_COR);
   int corrente = map(leituraCor, 0, 4095, 0, 100);
 
-  // --- 5. LÓGICA DE LEDS (Baseada na VIBRAÇÃO) ---
+  //5. LÓGICA DE LEDS (Baseada na VIBRAÇÃO)
   // Reseta
   digitalWrite(LED_VERDE, LOW);
   digitalWrite(LED_AMARELO, LOW);
@@ -100,19 +99,16 @@ void loop() {
     digitalWrite(LED_VERMELHO, HIGH); // Perigo (>45)
   }
 
-  // --- 6. LCD (MOSTRAR DADOS BRUTOS) ---
+  //6. LCD
+  lcd.print("PROVA - PRATICA")
   lcd.clear();
   lcd.setCursor(0,0);
-  // Ex: T:25.0 V:10%
   lcd.print("T:"); lcd.print(temp, 1); lcd.print(" V:"); lcd.print(vibracao); lcd.print("%");
   
   lcd.setCursor(0,1);
-  // Ex: C:12A St:OK
   lcd.print("C:"); lcd.print(corrente); lcd.print("A");
 
-  // --- 7. ENVIO FORMATO STRING SIMPLES (ANEXO 1) ---
-  // O Anexo pede: temperatura,vibracao,corrente
-  
+  //7. ENVIO FORMATO STRING
   String payload = String(temp, 1) + "," + String(vibracao) + "," + String(corrente);
 
   Serial.print("Enviando: ");
